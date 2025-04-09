71019e9d302f5cda3def483e16416f23808b6879
Tiberiu Serediuc <tiberiu.serediuc@arobs.com>
2025-04-09 12:50:24 +0300

    Added the main function with the main steps to be performed by the bootloader

--- main/bootloader_main.c
+++ main/bootloader_main.c
@@ -1,1 +1,23 @@
 // main/bootloader_main.c
+
+
+void main() {
+
+    // Print chip information for logging
+
+
+    // Init the flash memory
+
+
+    // Read the firmware metadata
+
+
+    // Validate the firmware 
+
+
+    // Start the application
+
+
+    // Error handling for invalid firmware or fauls
+
+}
